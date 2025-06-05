bcd291a9d2a06767add5b3631ad5fd3c37e6450c
10printhello <stuart.a.mainwaring@gmail.com>
2025-06-05 10:00:34 +0100

    updated formula

--- Formula/gnaw.rb
+++ Formula/gnaw.rb
@@ -1,33 +1,23 @@
 class Gnaw < Formula
-  desc "Blazingly fast, AI aware, drop-in replacement for grep"
-  homepage "https://gnaw.dev"
-  # Point to the source tarball for your Git tag:
-  url "https://github.com/10printhello/gnaw/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "MIT" 
+  desc "gnaw: blazingly fast, AI aware, drop-in replacement for grep"
+  homepage "https://github.com/10printhello/gnaw"
+  version "0.1.2"
 
-  # If your Rust code requires a minimum Rust version, you can specify:
-  depends_on "rust" => :build
+  if Hardware::CPU.intel?
+    url "https://gnaw-bin.s3.amazonaws.com/releases/gnaw-v0.1.2-darwin-x86_64.tar.gz"
+    sha256 "bdab4e0557e4cee579e7b39ca10c1b58c882a1ff1a9ade59a1b2e22d8bbb6f0f"
+  end
 
-  # Some Rust projects need additional build dependencies, e.g. pkg-config or openssl:
-  # depends_on "pkg-config" => :build
-  # depends_on "openssl@3"
+  if Hardware::CPU.arm?
+    url "https://gnaw-bin.s3.amazonaws.com/releases/gnaw-v0.1.2-darwin-aarch64.tar.gz"
+    sha256 "d8b6d3cf510a4ce1d82a71db7a43c1bb26a5b720f5db6497f01e080b20230353"
+  end
 
   def install
-    # If you want to fetch dependencies & build via Cargo:
-    system "cargo", "install", *std_cargo_args
-
-    # Alternatively, if you prefer to do a manual release build:
-    # system "cargo", "build", "--release", "--locked"
-    # Then:
-    # bin.install "target/release/gnaw"
+    bin.install "gnaw"
   end
 
   test do
-    # A simple test to verify installation. For example, if 'gnaw --version' prints something:
-    assert_match version.to_s, shell_output("#{bin}/gnaw --version")
-    # Or test basic functionality:
-    # (create a small input file, run `gnaw` on it, and verify output.)
+    assert_match "0.1.2", shell_output("#{bin}/gnaw --version")
   end
 end
-
